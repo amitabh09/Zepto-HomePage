c87e97c9aaf2828d22806c55be19d8dc0ae588e9
Amitabh Shahi <amitabhshahi09@gmail.com>
2025-01-28 23:46:28 +0530

    navbar started

---
+++ js/index.js
@@ -0,0 +1,5 @@
+// Simple JavaScript to show a message when the "Order Now" button is clicked
+document.getElementById('orderNowBtn').addEventListener('click', function() {
+    alert('Redirecting to the ordering page...');
+  });
+  
