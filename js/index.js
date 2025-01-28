6750cad50750ef4ce56c72f1871b082ab9890b4d
Amitabh Shahi <amitabhshahi09@gmail.com>
2025-01-29 00:21:10 +0530

    changed navbar

--- js/index.js
+++ js/index.js
@@ -1,5 +1,7 @@
-// Simple JavaScript to show a message when the "Order Now" button is clicked
-document.getElementById('orderNowBtn').addEventListener('click', function() {
-    alert('Redirecting to the ordering page...');
-  });
-  
+// document.getElementById('orderNowBtn').addEventListener('click', function() {
+//     alert('Redirecting to the ordering page...');
+//   });
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Zepto Clone Loaded");
+});
